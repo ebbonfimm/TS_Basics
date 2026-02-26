08be4deaed872fd8eeffef7f0aff7c59e63cee4c
ebbonfim <ebbonfim@sga.pucminas.br>
2026-02-26 18:48:03 -0300

    Aula de interfaces

--- learning/Interfaces.ts
+++ learning/Interfaces.ts
@@ -1,4 +1,19 @@
-// Interfaces
+// Interfaces - conhecido como Contrato
+// Aula https://www.youtube.com/watch?v=YwtudT9ji0A
+
+// declare global {
+//     interface console {
+//         sayHello(): void
+//     }
+// }
+
+// Object.assign(console, {
+//     sayHello(){
+//         console.log("Hello");
+//     }
+// })
+
+// console.sayHello();
 
 interface UserWallet {
     coins?: number;
@@ -33,3 +48,36 @@ const weedub = createUser("weedub")
 const weedub_wallet: UserWallet = { coins: 10 }
 
 updateWallet(weedub, weedub_wallet)
+
+
+interface Admin extends User{
+    // name: string,
+    // createdAt: Date;
+    // wallet?: UserWallet;
+    ban(user: User): void;
+    kick(user: User): void;
+}
+
+function promoteUser(user: User): Admin {
+    return {
+        ...user,
+        ban(userToBan) {
+            console.log(userToBan, "foi banido por", this.name)
+        },
+        kick(userToKick) {
+            console.log(userToKick, "foi expulso por", this.name)
+        }
+    }
+}
+
+const admWeedub = promoteUser(weedub)
+
+admWeedub.kick(weedub)
+
+
+function adminAction(admin: Admin): string {
+    return ""
+}
+
+// adminAction(weedub)
+adminAction(admWeedub)

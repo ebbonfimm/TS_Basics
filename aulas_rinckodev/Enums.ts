c92bb08046807f1025a67a69d971c506aa4df07d
ebbonfim <ebbonfim@sga.pucminas.br>
2026-03-01 17:55:29 -0300

    Rincko Dev Curso - Enuns

---
+++ aulas_rinckodev/Enums.ts
@@ -0,0 +1,29 @@
+// Enums
+
+enum Direction {
+    North=1,
+    South,
+    East,
+    West
+}
+
+const pos = {
+    x: 100,
+    y: 200,
+    direction: Direction.North
+}
+
+enum ClothingSize {
+    ExtraSmall = "PP",
+    Small = "P",
+    Medium = "M",
+    Large = "G",
+    ExtraLarge = "GG",
+}
+
+const cloth = {
+    colro: "green",
+    size: ClothingSize.Large
+}
+
+
